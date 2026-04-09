2fc60442d017143f4464dd2d272f785e975e8794
koansito <chronwasclosed@gmail.com>
2026-04-09 07:21:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 import base64
 from openai import OpenAI
+from PIL import Image
 
 # Function to encode the image to base64
 def encode_image(image_file):
@@ -13,7 +14,9 @@ st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_s
 st.title("Análisis de Imagen:🤖🏞️")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
+image = Image.open('carlos.jpg')
 
+st.image(image,width=3000)
 
 # Retrieve the OpenAI API Key from secrets
 api_key = os.environ['OPENAI_API_KEY']
